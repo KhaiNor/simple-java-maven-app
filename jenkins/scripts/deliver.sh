024e4c06e6d97597d03be70e70e10a132dd08286
KhaiNor <155954195+KhaiNor@users.noreply.github.com>
2024-05-20 16:58:50 +0800

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -11,14 +11,14 @@ set +x
 echo 'The following command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-NAME=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.name | sed -e 's/[^[:print:]]//g')
+NAME=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.name | awk 'NR==1 {print}')
 echo "Project Name: ${NAME}"
 set +x
 
 echo 'The following command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
-VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version | sed -e 's/[^[:print:]]//g')
+VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version | awk 'NR==1 {print}')
 echo "Project Version: ${VERSION}"
 set +x
 
